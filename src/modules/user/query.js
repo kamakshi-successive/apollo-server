0e294ffdda0fa15435774886cb9c53089314c430
kamakshikumari <kamakshi.kumari@successive.tech>
2021-02-03 12:16:26 +0530

    Completed

--- src/modules/user/query.js
+++ src/modules/user/query.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
 export default {
   getMyProfile: async (parent, args, context) => {
+    console.log('args', args);
     const { dataSources: { userAPI } } = context;
     const response = await userAPI.getMe();
+    // eslint-disable-next-line no-console
+    console.log('respose getmyprofile', response);
     return response.data;
   }
 };
